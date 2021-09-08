63454a812c460ac4efda3cfbcca78b3e95419803
Brian Haan <brianchaan@gmail.com>
2021-09-08 14:29:42 -0700

    Updated bootstrap

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Process for setting up a new mac:
 
-1. Ctrl+Space and type Terminal to open the default Terminal. Then type ```git clone https://github.com/brianchaan/dotfiles``` to clone this repository.
-1. Run ```source ~/dotfiles/bootstrap.sh``` to symlink dotfiles, set up directories, install Homebrew, etc.
+1. Open Safari and Google *Install Homebrew*. Open the Homebrew site and copy the bash command.
+    - Ctrl+Space and type *Terminal* to open the default Terminal.
+    - Paste the command. This will install both Homebrew and the Xcode command line tools.
+1. In the Terminal, run ```cd ~ && git clone https://github.com/brianchaan/dotfiles``` to clone this repository.
+1. Run ```source ~/dotfiles/bootstrap.sh``` to symlink dotfiles, set up directories, etc.
 
 
 1. Customize iTerm by loading init/com.googlecode.iterm2.plist into iTerm preferences
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")";
 
-# git pull origin master;
+git pull origin main;
 
 # function doIt() {
 # 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
@@ -14,26 +14,45 @@
 # if [ "$1" == "--force" -o "$1" == "-f" ]; then
 # 	doIt;
 # else
-# 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-# 	echo "";
-# 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-# 		doIt;
-# 	fi;
+
 # fi;
-# unset doIt;
-#TODO the above should symlink instead of copy each dotfile
+
+# Add symlinks to dotfiles
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/N) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  ln -sf $PWD/.vim ~/.vim
+  ln -sf $PWD/Themes ~/Themes
+  ln -sf $PWD/.aliases ~/.aliases
+  ln -sf $PWD/.bash_profile ~/.bash_profile
+  ln -sf $PWD/.bash_prompt ~/.bash_prompt
+  ln -sf $PWD/.bashrc ~/.bashrc
+  ln -sf $PWD/.curlrc ~/.curlrc
+  ln -sf $PWD/.editorconfig ~/.editorconfig
+  ln -sf $PWD/.exports ~/.exports
+  ln -sf $PWD/.extras ~/.extras
+  ln -sf $PWD/.functions ~/.functions
+  ln -sf $PWD/.gitattributes ~/.gitattributes
+  ln -sf $PWD/.gitconfig ~/.gitconfig
+  ln -sf $PWD/.gvimrc ~/.gvimrc
+  ln -sf $PWD/.hushlogin ~/.hushlogin
+  ln -sf $PWD/.inputrc ~/.inputrc
+  ln -sf $PWD/.npmrc ~/.npmrc
+  ln -sf $PWD/.osx ~/.osx
+  ln -sf $PWD/.path ~/.path
+  ln -sf $PWD/.screenrc ~/.screenrc
+  ln -sf $PWD/.vimrc ~/.vimrc
+  ln -sf $PWD/.wgetrc ~/.wgetrc
+fi;
 
 # Folders
-mkdir ~/code
-mkdir ~/code/h1
-mkdir /var/www
+mkdir -p ~/code/h1
+mkdir -p /var/www
 
 # Set Bash as default shell
 echo /usr/local/bin/bash | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/bash
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Open dotfiles README to go through installation checklist
 open https://www.github.com/brianchaan/dotfiles
